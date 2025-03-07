86014e2ef253dd176e27c6f835fa506824de54e4
Hellen <maryreisl42@gmail.com>
2025-03-07 15:08:55 -0300

    swagger configuração

---
+++ src/main/java/br/com/Loja/Config/SwaggerController.java
@@ -0,0 +1,16 @@
+package br.com.Loja.Config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class SwaggerController {
+
+	@GetMapping
+	public String redirectToSwagger() {
+        return "redirect:/swagger-ui.html";
+    }
+}
